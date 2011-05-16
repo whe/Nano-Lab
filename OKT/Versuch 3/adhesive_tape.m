548b3aec31510108472382276f7858708c7928ea
Sebastian Heunisch <Sebastian.Heunisch@student.kit.edu>
2011-05-16 10:15:56 +0200

    bla

--- OKT/Versuch 3/adhesive_tape.m
+++ OKT/Versuch 3/adhesive_tape.m
@@ -10,12 +10,13 @@ k0 = 2*pi/lambda0;
 delta_k = 2*pi*delta_lambda/lambda0^2;
 sdelta_k = 12.5e9*2*pi/3e8;
 
-
-
+m = length(Rk);
+sdelta_z = 2*pi/(2*1.5*m*sdelta_k);
 for m = (1:length(Rk)-1)
     k(m) = k0 - delta_k/2 + m*sdelta_k;
-    sdelta_z(m) = 2*pi/(2*m*sdelta_k); %space between the step in the z-domain.(cf. S 62in Izatt/Choma
+     %space between the step in the z-domain.(cf. S 62in Izatt/Choma
     
     z(1) = 0;
-    z(m+1) =z(m) +sdelta_z(m);
+    z(m+1) =z(m) +sdelta_z;
 end
+plot(z,abs(S))
